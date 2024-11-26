e0ebef6bc6ea32057493e1c4a4d90aa8927ab047
gervasius <gervasiuswisely@gmail.com>
2024-11-26 09:34:18 +0700

    tes recaptcha

--- api/server.js
+++ api/server.js
@@ -10,42 +10,75 @@ app.use(cors());
 app.use(multer().none()); // Menangani data multipart/form-data
 
 // Mengatur kredensial Basic Authentication
-const login = process.env.login;
-const password = process.env.password;
-const credentials = Buffer.from(`${login}:${password}`).toString('base64');
-const authHeader = `Basic ${credentials}`;
+// const login = process.env.login;
+// const password = process.env.password;
+// const credentials = Buffer.from(`${login}:${password}`).toString('base64');
+// const authHeader = `Basic ${credentials}`;
+
+// app.post('/api/data', async (req, res) => {
+//     console.log('Request Body:', req.body);
+//   try {
+//     // Membuat instance FormData
+//     const form = new FormData();
+
+//     // Menambahkan data dari body request ke FormData
+//     Object.keys(req.body).forEach(key => {
+//       form.append(key, req.body[key]);
+//     });
+
+//     // Mengirim permintaan POST ke API eksternal dengan FormData dan Basic Authentication
+//     const response = await fetch(process.env.url, {
+//       method: 'POST',
+//       headers: {
+//         ...form.getHeaders(), // Menyertakan header yang diperlukan untuk FormData
+//         'Authorization': authHeader // Menyertakan header Basic Authentication
+//       },
+//       body: form // Mengirim FormData sebagai body permintaan
+//     });
+
+//     // Mendapatkan data dari API eksternal
+//     const data = await response.json();
+
+//     // Mengirimkan data kembali ke klien
+//     res.json(data);
+//   } catch (error) {
+//     // Menangani kesalahan dan mengirimkan respons error
+//     console.error(error);
+//     res.status(500).json({ error: 'Failed to fetch data' });
+//   }
+// });
 
 app.post('/api/data', async (req, res) => {
-    console.log('Request Body:', req.body);
-  try {
-    // Membuat instance FormData
-    const form = new FormData();
+  console.log('tikus')
+  // console.log('Request Body:', req.body);
+try {
+  // Membuat instance FormData
+  // const form = new FormData();
 
-    // Menambahkan data dari body request ke FormData
-    Object.keys(req.body).forEach(key => {
-      form.append(key, req.body[key]);
-    });
+  // Menambahkan data dari body request ke FormData
+  // Object.keys(req.body).forEach(key => {
+  //   form.append(key, req.body[key]);
+  // });
 
-    // Mengirim permintaan POST ke API eksternal dengan FormData dan Basic Authentication
-    const response = await fetch(process.env.url, {
-      method: 'POST',
-      headers: {
-        ...form.getHeaders(), // Menyertakan header yang diperlukan untuk FormData
-        'Authorization': authHeader // Menyertakan header Basic Authentication
-      },
-      body: form // Mengirim FormData sebagai body permintaan
-    });
+  // Mengirim permintaan POST ke API eksternal dengan FormData dan Basic Authentication
+  const response = await fetch(process.env.url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(response,req.body),
+  });
 
-    // Mendapatkan data dari API eksternal
-    const data = await response.json();
+  // Mendapatkan data dari API eksternal
+  const data = await response.json();
 
-    // Mengirimkan data kembali ke klien
-    res.json(data);
-  } catch (error) {
-    // Menangani kesalahan dan mengirimkan respons error
-    console.error(error);
-    res.status(500).json({ error: 'Failed to fetch data' });
-  }
+  // Mengirimkan data kembali ke klien
+  res.json(data);
+} catch (error) {
+  // Menangani kesalahan dan mengirimkan respons error
+  console.error(error);
+  res.status(500).json({ error: 'Failed to fetch data' });
+}
 });
 
 module.exports = app;
